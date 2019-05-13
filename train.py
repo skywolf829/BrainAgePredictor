58b722bc8acc112a1cc3d34405d811548fd20983
Skylar Wurster <swwurster@gmail.com>
2019-05-13 15:57:32 -0400

    Creates new combined models. New code structure, more clean. New powerpoint for analysis of new model predictions.

--- train.py
+++ train.py
@@ -1,8 +1,5 @@
 import numpy as np
 from sklearn import svm
-import matplotlib.pyplot as ptl
-import csv
-import time
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.metrics import r2_score
@@ -20,134 +17,33 @@ from sklearn import linear_model
 from sklearn.model_selection import cross_val_score, ShuffleSplit
 from sklearn.utils import shuffle
 from scipy import stats
+import utility_functions
 
-def getCols(data, col):
-    newData = []
-    currentRow = 0
-    for row in data:
-        currentCol = 0
-        newRow = []
-        for item in row:
-            if currentCol in col:
-                newRow.append(item)
-            currentCol = currentCol + 1
-        newData.append(newRow)
-        currentRow = currentRow + 1
-    return newData
+patientToGroup, patientToAge, patientToFeatures = utility_functions.loadCSVDictionary('cortical_data.csv')
+patientToGroup, patientToAge, patientToFeatures = utility_functions.loadCSVDictionary('subcortical_data.csv', patientToGroup, patientToAge, patientToFeatures)
+patientToGroup, patientToAge, patientToFeatures = utility_functions.loadCSVDictionary('DTI_data.csv', patientToGroup, patientToAge, patientToFeatures)
 
-def dropCols(data, dropCols):
-    newData = []
-    currentRow = 0
-    for row in data:
-        currentCol = 0
-        newRow = []
-        for item in row:
-            if not currentCol in dropCols:
-                newRow.append(item)
-            currentCol = currentCol + 1
-        newData.append(newRow)
-        currentRow = currentRow + 1
-    return newData
+IDs, groups, ages, features = utility_functions.getAgesAndFeaturesForGroups(patientToGroup, patientToAge, patientToFeatures, [0])
+IDs1, groups1, ages1, features1 = utility_functions.getAgesAndFeaturesForGroups(patientToGroup, patientToAge, patientToFeatures, [1])
+IDs2, groups2, ages2, features2 = utility_functions.getAgesAndFeaturesForGroups(patientToGroup, patientToAge, patientToFeatures, [2])
+IDs3, groups3, ages3, features3 = utility_functions.getAgesAndFeaturesForGroups(patientToGroup, patientToAge, patientToFeatures, [3])
 
-def calcAverageDiff(arr1, arr2):
-    diff = 0
-    i = 0
-    while i < len(arr1):
-        diff = diff + abs(arr1[i] - arr2[i])
-        i = i + 1
-    diff = diff / len(arr1)
-    return diff
+IDs_all, groups_all, ages_all, features_all = utility_functions.getAgesAndFeaturesForGroups(patientToGroup, patientToAge, patientToFeatures, [0, 1 , 2, 3])
 
-def printWithLines(arr):
-    for item in arr:
-        print(item)
+#features_all = np.concatenate(features_all[0], utility_functions.loadCSV('subcorticalData.csv')[5][0], utility_functions.loadCSV('DTIdata.csv')[5][0])
+#ages_all = np.concatenate(ages_all[0], utility_functions.loadCSV('subcorticalData.csv')[10], utility_functions.loadCSV('DTIdata.csv')[10])
 
-grouping = 10
-features = []
-features_1 = []
-features_2 = []
-features_3 = []
-features_1to3 = []
-features_all = []
-age_groups = []
-age_groups_1 = []
-age_groups_2 = []
-age_groups_3 = []
-ages = []
-ages_1 = []
-ages_2 = []
-ages_3 = []
-ages_1to3 = []
-ages_all = []
-headers = []
-with open('training_use_0_dataset.csv') as file:
-    csv = csv.reader(file)
-    r = 0
-    for row in csv:
-        control = 0
-        c = 0
-        patientData = []        
-        for item in row:
-            if r == 0:
-                if item != None and item != "" and item != " ":
-                    headers.append(item)
-            else:
-                if item != None and item != "" and item != " ":
-                    if c == 1:
-                        if item == "0":
-                            control = 0
-                        if item == "1":
-                            control = 1
-                        if item == "2":
-                            control = 2
-                        if item == "3":
-                            control = 3
-                    elif c == 2:
-                        age_bot = int(float(item) / grouping)
-                        if control == 0:
-                            age_groups.append(str(age_bot * grouping) + " to " + str(age_bot * grouping + grouping - 1)) 
-                            ages.append(float(item))
-                        elif control == 1:
-                            age_groups_1.append(str(age_bot * grouping) + " to " + str(age_bot * grouping + grouping - 1)) 
-                            ages_1.append(float(item)) 
-                        elif control == 2:
-                            age_groups_2.append(str(age_bot * grouping) + " to " + str(age_bot * grouping + grouping - 1)) 
-                            ages_2.append(float(item)) 
-                        elif control == 3:
-                            age_groups_3.append(str(age_bot * grouping) + " to " + str(age_bot * grouping + grouping - 1)) 
-                            ages_3.append(float(item)) 
-                    if c >= 4 and c <= 28:
-                        patientData.append(float(item))
-                        
-                c = c + 1
-        if len(patientData) > 0 and r != 0:
-            if control == 0:
-                features.append(np.array(patientData))
-            elif control == 1:
-                features_1.append(np.array(patientData))
-            elif control == 2:
-                features_2.append(np.array(patientData))
-            elif control == 3:
-                features_3.append(np.array(patientData))
-        r = r + 1
-features_1to3 = np.concatenate((features_1, features_2, features_3))
-features_all = np.concatenate((features, features_1, features_2, features_3))
-ages_1to3 = np.concatenate((ages_1, ages_2, ages_3))
-ages_all = np.concatenate((ages, ages_1, ages_2, ages_3))
-
-params = {'C': stats.expon(scale=0.00001), 'gamma': stats.expon(scale=0.1), 'kernel': ['linear']}
- 
-
+params = {'C': stats.expon(scale=10), 'gamma': ["scale"], 'coef0':[0,1,10], 'kernel': ['sigmoid']}
 
 # REGRESSORS
-#classifier = svm.SVR(kernel='linear', C=0.000001)
+#classifier = svm.SVR(kernel='rbf', C=50, gamma="scale")
 #classifier = LinearRegression()
 #classifier = svm.SVR(kernel='linear', C=.0000001, gamma='scale')
 #classifier = svm.SVR(kernel='poly', C=0.1, gamma='scale', degree=10, coef0=1, epsilon=1)
-#classifier = MLPRegressor(hidden_layer_sizes=(100), solver="adam", activation='logistic', tol=0.00001, alpha=0.0001, max_iter=1000000)
+#classifier = MLPRegressor(hidden_layer_sizes=(100, 100, 100), solver="adam", activation='logistic', tol=0.00001, alpha=0.0001, max_iter=1000000)
 #classifier = DecisionTreeRegressor()
 #classifier = GridSearchCV(svm.SVR(), params, cv=5)
-classifier = RandomizedSearchCV(svm.SVR(), params, cv = 5, n_iter=1000, n_jobs=-1)
+#classifier = RandomizedSearchCV(svm.SVR(), params, cv = 5, n_iter=1000, n_jobs=-1)
 #classifier = linear_model.LinearRegression()
 #classifier = linear_model.RidgeCV(alphas=np.logspace(-6, 6, 13))
 #classifier = linear_model.Lasso(alpha=0.1, max_iter=1000)
@@ -155,7 +51,7 @@ classifier = RandomizedSearchCV(svm.SVR(), params, cv = 5, n_iter=1000, n_jobs=-
 #classifier = linear_model.LarsCV()
 #classifier = linear_model.BayesianRidge()
 #classifier = linear_model.SGDRegressor(max_iter=10000)
-#classifier = linear_model.Perceptron(max_iter=10000)
+classifier = linear_model.Perceptron(max_iter=10000)
 #classifier = linear_model.HuberRegressor(max_iter=10000)
 
 # CLASSIFIERS
@@ -167,80 +63,28 @@ classifier = RandomizedSearchCV(svm.SVR(), params, cv = 5, n_iter=1000, n_jobs=-
 #classifier = NearestCentroid()
 #classifier = RandomizedSearchCV(svm.SVC(), params, cv=3, n_iter=100, n_jobs=-1)
 
-i = 0
-iters = 1
-
-average_score_1 = 0
-average_score_2 = 0
-average_score_3 = 0
-average_age_diff_1 = 0
-average_age_diff_2 = 0
-average_age_diff_3 = 0
-
 sc_X = StandardScaler()
-sc_y = StandardScaler()
-pca = PCA(n_components=None)
+sc_X.fit(features)
 
+features = sc_X.transform(features)
+features1 = sc_X.transform(features1)
+features2 = sc_X.transform(features2)
+features3 = sc_X.transform(features3)
+features_all = sc_X.transform(features_all)
 
-#ages = sc_y.fit_transform(np.array(ages)[:, np.newaxis]).reshape(len(ages))
-#features = pca.fit_transform(features)
-#features = sc_X.fit_transform(features)
-#print(np.mean(features, axis=0))
-#print(np.min(features, axis=0))
-#print(np.max(features, axis=0))
-#print(np.std(features, axis=0))
-#np.histogram(np.array(features).take(1))
+classifier.fit(features, ages)
+print("Score on training data: " + str(classifier.score(features, ages)))
+print("Score on group 1: " + str(classifier.score(features1, ages1)))
+print("Score on group 2: " + str(classifier.score(features2, ages2)))
+print("Score on group 3: " + str(classifier.score(features3, ages3)))
 
+y_predAll = classifier.predict(features_all)
+y_pred1 = classifier.predict(features1)
+y_pred2 = classifier.predict(features2)
+y_pred3 = classifier.predict(features3)
 
-while i < iters:
-    #features, ages = shuffle(features, ages, random_state=0)
-    #X_train, X_test, y_train, y_test = train_test_split(features, ages, test_size = 0.20) 
-    
-    classifier.fit(features, ages)
-    print("Score on training data " + str(classifier.score(features, ages)))
-    y_predAll = classifier.predict(features_all)
-    score1 = classifier.score(features_1, ages_1)
-    score2 = classifier.score(features_2, ages_2)
-    score3 = classifier.score(features_3, ages_3)
-    y_pred1 = classifier.predict(features_1)
-    y_pred2 = classifier.predict(features_2)
-    y_pred3 = classifier.predict(features_3)
-    average_age_diff_1 = average_age_diff_1 + calcAverageDiff(y_pred1, ages_1)
-    average_age_diff_2 = average_age_diff_2 + calcAverageDiff(y_pred2, ages_2)
-    average_age_diff_3 = average_age_diff_3 + calcAverageDiff(y_pred3, ages_3)
+print("Avg age diff on group 1: " + str(utility_functions.calcAverageDiff(y_pred1, ages1)))
+print("Avg age diff on group 2: " + str(utility_functions.calcAverageDiff(y_pred2, ages2)))
+print("Avg age diff on group 3: " + str(utility_functions.calcAverageDiff(y_pred3, ages3)))
 
-    average_score_1 = average_score_1 + score1
-    average_score_2 = average_score_2 + score2
-    average_score_3 = average_score_3 + score3
-    #average_age_diff = average_age_diff + age_diff
-    print(classifier.best_params_)
-    #("Finished step " + str(i+1) + "/" + str(iters) + " score1= " + str(score1) + " score2= " + str(score2) + " score3= " + str(score3)) 
-    #print("Age diff= " + str(age_diff))
-    i = i + 1
-
-
-average_score_1 = average_score_1 / iters
-average_score_2 = average_score_2 / iters
-average_score_3 = average_score_3 / iters
-average_age_diff_1 = average_age_diff_1 / iters
-average_age_diff_2 = average_age_diff_2 / iters
-average_age_diff_3 = average_age_diff_3 / iters
-#average_age_diff = average_age_diff / iters
-#print("Complete. Avg score= " + str(average_r2))
-#print("Average age diff= " + str(average_age_diff))
-print("Complete.")
-print("Avg score1= " + str(average_score_1))
-print("Avg score2= " + str(average_score_2))
-print("Avg score3= " + str(average_score_3))
-print("Avg age diff1= " + str(average_age_diff_1))
-print("Avg age diff2= " + str(average_age_diff_2))
-print("Avg age diff3= " + str(average_age_diff_3))
-#print(confusion_matrix(y_test, y_pred))
-#print(classification_report(y_test, y_pred))
-
-#print("break")
-#printWithLines(ages_all)
-#print("break")
-#printWithLines(y_predAll)
-#print("break")
-#printWithLines(y_predAll - ages_all )
+utility_functions.printWithLines(y_predAll - ages_all)
---
+++ utility_functions.py
@@ -0,0 +1,164 @@
+import csv
+import numpy as np
+
+def loadCSV(filename):
+    grouping = 10
+    features = []
+    features_1 = []
+    features_2 = []
+    features_3 = []
+    features_all = []
+    age_groups = []
+    age_groups_1 = []
+    age_groups_2 = []
+    age_groups_3 = []
+    ages = []
+    ages_1 = []
+    ages_2 = []
+    ages_3 = []
+    ages_all = []
+    headers = []
+    with open(filename) as file:
+        csvFile = csv.reader(file)
+        r = 0
+        for row in csvFile:
+            control = 0
+            c = 0
+            patientData = []        
+            for item in row:
+                if r == 0:
+                    if item != None and item != "" and item != " " and c >= 5:
+                        headers.append(item)
+                else:
+                    if item != None and item != "" and item != " ":
+                        if c == 1:
+                            if item == "0":
+                                control = 0
+                            if item == "1":
+                                control = 1
+                            if item == "2":
+                                control = 2
+                            if item == "3":
+                                control = 3
+                        elif c == 2:
+                            age_bot = int(float(item) / grouping)
+                            if control == 0:
+                                age_groups.append(str(age_bot * grouping) + " to " + str(age_bot * grouping + grouping - 1)) 
+                                ages.append(float(item))
+                            elif control == 1:
+                                age_groups_1.append(str(age_bot * grouping) + " to " + str(age_bot * grouping + grouping - 1)) 
+                                ages_1.append(float(item)) 
+                            elif control == 2:
+                                age_groups_2.append(str(age_bot * grouping) + " to " + str(age_bot * grouping + grouping - 1)) 
+                                ages_2.append(float(item)) 
+                            elif control == 3:
+                                age_groups_3.append(str(age_bot * grouping) + " to " + str(age_bot * grouping + grouping - 1)) 
+                                ages_3.append(float(item)) 
+                        if c >= 5:
+                            patientData.append(float(item))
+                            
+                c = c + 1
+            if len(patientData) > 0 and r != 0:
+                if control == 0:
+                    features.append(np.array(patientData))
+                elif control == 1:
+                    features_1.append(np.array(patientData))
+                elif control == 2:
+                    features_2.append(np.array(patientData))
+                elif control == 3:
+                    features_3.append(np.array(patientData))
+            r = r + 1
+    
+    features_all = np.concatenate((features, features_1, features_2, features_3))
+    ages_all = np.concatenate((ages, ages_1, ages_2, ages_3))
+
+    return headers, features, features_1, features_2, features_3, features_all, ages, ages_1, ages_2, ages_3, ages_all
+
+def loadCSVDictionary(filename, patientToGroup = {}, patientToAge = {}, patientToFeatures={}):
+    patientToGroup = {}
+    patientToAge = {}
+    headers = []
+    with open(filename) as file:
+        csvFile = csv.reader(file)
+        r = 0
+        for row in csvFile:
+            c = 0
+            patientData = []        
+            for item in row:
+                if r == 0:
+                    if item != None and item != "" and item != " " and c >= 5:
+                        headers.append(item)
+                else:
+                    if item != None and item != "" and item != " ":
+                        if c == 0 and not row[0] in patientToFeatures:
+                            patientToFeatures[row[0]] = []
+                        elif c == 1:
+                            patientToGroup[row[0]] = int(item)
+                        elif c == 2:
+                            patientToAge[row[0]] = int(item)
+                        elif c >= 4:
+                            patientData.append(float(item))
+                            
+                c = c + 1
+            if len(patientData) > 0 and r != 0:
+                patientToFeatures[row[0]] = np.ndarray.tolist(np.concatenate((patientToFeatures[row[0]], patientData), axis=0))
+            r = r + 1 
+    return patientToGroup, patientToAge, patientToFeatures
+
+def getAgesAndFeaturesForGroups(patientGroups, patientAges, patientFeatures, groups):
+    patientIDs = []
+    patientGroupsList = []
+    patientAgesList = []
+    patientFeaturesList = []
+
+    for patient in patientGroups:
+        if patientGroups[patient] in groups:
+            patientIDs.append(patient)
+
+    for patientID in patientIDs:
+        patientGroupsList.append(patientGroups[patientID])
+        patientAgesList.append(patientAges[patientID])
+        patientFeaturesList.append(patientFeatures[patientID])
+
+    return patientIDs, patientGroupsList, patientAgesList, patientFeaturesList
+
+def getCols(data, col):
+    newData = []
+    currentRow = 0
+    for row in data:
+        currentCol = 0
+        newRow = []
+        for item in row:
+            if currentCol in col:
+                newRow.append(item)
+            currentCol = currentCol + 1
+        newData.append(newRow)
+        currentRow = currentRow + 1
+    return newData
+
+def dropCols(data, dropCols):
+    newData = []
+    currentRow = 0
+    for row in data:
+        currentCol = 0
+        newRow = []
+        for item in row:
+            if not currentCol in dropCols:
+                newRow.append(item)
+            currentCol = currentCol + 1
+        newData.append(newRow)
+        currentRow = currentRow + 1
+    return newData
+
+def calcAverageDiff(arr1, arr2):
+    diff = 0
+    i = 0
+    while i < len(arr1):
+        diff = diff + arr1[i] - arr2[i]
+        i = i + 1
+    diff = diff / len(arr1)
+    return diff
+
+def printWithLines(arr):
+    for item in arr:
+        print(item)
